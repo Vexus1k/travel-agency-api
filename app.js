ef2f2590bb5c4e02dbba2470829728f4425e635a
Jakub Popiołek <jpopiole@Jakubs-MacBook-Air.local>
2023-06-21 12:53:27 +0200

    Added delete method and getting from db

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
+require('dotenv').config();
 const express = require('express');
-require("dotenv").config();
-const app = express();
 const bodyParser = require('body-parser');
-const { getOffersData } = require("./firebaseModule");
+const { getOffersData } = require('./firebaseModule.js');
+const {deleteElement} = require("./firebaseModule");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, ()=> console.log("listening in 3000"));
+app.use(bodyParser.json());
+
 app.use( function (req, res, next) {
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', '*');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-    //Off preflight request.
     res.setHeader('Access-Control-Max-Age', '600')
 
     next();
 }, bodyParser.json());
 app.use(express.static('public'));
 app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/getDataFromFirebaseDB', (request, response ) => {
-    const { databaseName } = request.query;
+app.get('/getDataFromFirebaseDB', (req, res ) => {
+    const { databaseName } = req.query;
 
     getOffersData(databaseName)
         .then((data) => {
             console.log(data);
 
-            response.json(data);
+            res.json(data);
         })
         .catch((error) => {
             console.error('Error retrieving data:', error);
-            response.status(500).json({ error: 'An error occurred' });
+            res.status(500).json({ error: 'An error occurred' });
         });
+});
+
+app.delete('/delete/:collectionName/:elementId', (req, res) => {
+    const collectionName = req.params.collectionName;
+    const elementId = req.params.elementId;
+
+
+    deleteElement(collectionName, elementId)
+        .then(() => {
+            res.status(200).json({ message: 'Element successfully deleted from the database.' });
+        })
+        .catch((error) => {
+            console.error('Error deleting element:', error);
+            res.status(500).json({ error: 'An error occurred while deleting the element.' });
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
 });
--- firebaseModule.js
+++ firebaseModule.js
@@ -1,6 +1,6 @@
-const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
-
+const firebase = require('firebase/app');
+require('firebase/firestore');
+const { getFirestore, collection, getDocs, doc, deleteDoc } = require('firebase/firestore/lite');
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
@@ -9,13 +9,32 @@ const firebaseConfig = {
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID
 };
-
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+const admin = require('firebase-admin');
+const fs = require('fs');
+const templateFile = fs.readFileSync('serviceAccountKey.json', 'utf8');
+let jsonData = JSON.parse(templateFile);
 
-const getOffersData = async (database) => {
+jsonData.type = process.env.TYPE;
+jsonData.project_id = process.env.PROJECT_ID;
+jsonData.private_key_id = process.env.PRIVATE_KEY_ID;
+jsonData.private_key = process.env.PRIVATE_KEY;
+jsonData.client_email = process.env.CLIENT_EMAIL;
+jsonData.client_id = process.env.CLIENT_ID;
+jsonData.auth_uri = process.env.AUTH_URI;
+jsonData.token_uri = process.env.TOKEN_URI;
+jsonData.auth_provider_x509_cert_url = process.env.AUTH_PROVIDER_X509_CERT_URL;
+jsonData.client_x509_cert_url = process.env.CLIENT_X509_CERT_URL;
+jsonData.universe_domain = process.env.UNIVERSE_DOMAIN;
+
+admin.initializeApp({
+    credential: admin.credential.cert(jsonData)
+});
+
+const getOffersData = async (collectionName) => {
     try {
-        const querySnapshot = await getDocs(collection(firestore, database));
+        const querySnapshot = await getDocs(collection(firestore, collectionName));
         const offersData = [];
 
         querySnapshot.forEach((doc) => {
@@ -30,4 +49,10 @@ const getOffersData = async (database) => {
     }
 };
 
-module.exports = { getOffersData };
+function deleteElement(collectionName, elementId) {
+    const elementRef = doc(firestore, `${collectionName}/${elementId}`);
+
+    return deleteDoc(elementRef);
+}
+
+module.exports = { getOffersData, deleteElement };
